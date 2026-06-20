01d1e62cecb4d8e089a880f6cb554900c2431bb1
Nocturn le Chat <zimlin.nazar@yandex.ru>
2026-06-20 17:44:38 +0700

    Added basic infrastructure

--- classes.py
+++ classes.py
@@ -1,28 +1,32 @@
 # Здесь хранятся классы для каталогизации книг и читателей, а также инструментарий для работы с ними
 from json import *
+from os import path
+
+class json:
+    def encoder(book):
+        if not type(book) == Book: raise TypeError()
+        else: return {"GENRE": book.genre, "AUTHOR": book.author, "TITLE": book.title, "YEAR": book.year, "STATUS": book.status }
 
 class Book:
-    genre = ""
-    author = ""
-    title = ""
-    year = ""
-    pass
-
-    def add(title, author, year, genre):
+    def create(genre=None, author=None, title=None, year=None):
+        if any([genre, author, title, year]) in [None, "", " "]: raise ValueError(print("ERROR: missing paramethers"))
+        if not year.strip().isnumeric(): raise ValueError("ERROR: year must conntain numbers only")
         book = Book()
-
-        book.genre, book.author, book.title, book.year == genre, author, title, year
-        book.status = "Free"      
-
-        with open("books_data.json", mode="r") as file:
-            base = load(file)
-            base[len(base.keys)+1] = book
-            
-    
-    def delete(id):
-        with open("books_data.json", "a+") as file:
-            base = load(file)
-            if not id in base.keys: raise ValueError
-            else: del base[id]
-
-Book.add("K@t3h1z1$ @nt1$3xu@l@", "Nocturn le Chat", "1337", "Must-have")
+        book.genre = genre.strip()
+        book.author = author.strip()
+        book.title = title.strip()
+        book.year = year.strip()
+        book.status = "FREE"
+        return book
+        
+    def add_base(book):
+        if not type(book) == Book: raise TypeError()
+        book = json.encoder(book)
+        if not path.exists("book_database.json"):
+            database = {1: book}
+        else:
+            with open("book_database.json") as file:
+                database = load(file)
+                database[int(max(database.keys()))+1] = book
+        with open("book_database.json", "w") as file:
+            dump(database, file, indent=4, ensure_ascii=False)
