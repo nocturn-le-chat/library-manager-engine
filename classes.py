b83e06a33fc5b5e4d3fe9942a8885d9c13cda3e9
Nocturn le Chat <zimlin.nazar@yandex.ru>
2026-06-25 11:58:30 +0700

    Optimized operations with objects

--- classes.py
+++ classes.py
@@ -2,93 +2,95 @@
 from json import *
 from os import path
 
-class json:
-    def encoder(book):
-        if not type(book) == Book: raise TypeError()
-        else: return {"genre": book.genre, "author": book.author, "title": book.title, "year": book.year, "status": book.status }
-    
-    def decoder(dictionary):
-        if not type(dictionary) == dict: raise TypeError()
-        if not list(dictionary.keys()) == ["genre", "author", "title", "year", "status"]: raise ValueError()
 
-        return Book.create(dictionary["genre"], dictionary["author"], dictionary["title"], dictionary["year"], status=dictionary["status"])
+def hash(password, *mode):
+    if mode == ("encrypt",):
+        p_hash = 0
+        for k in range(len(password)):
+            p_hash += ord(password[k])*65599**(len(password) - k)
+            return p_hash
+    elif mode == ("decrypt",):
+            
+        return None
 
-class Book:
-    def create(genre=None, author=None, title=None, year=None, **kwargs):
-        if not list(kwargs.keys()) in [[], ["status"]]: raise ValueError()
-        status = kwargs["status"] if "status" in kwargs.keys() else "FREE"
-
-        if any([genre, author, title, year]) in [None, "", " "]: raise ValueError(print("ERROR: missing paramethers"))
-        if not year.strip().isnumeric(): raise ValueError("ERROR: year must conntain numbers only")
-        book = Book()
-        book.genre  = genre.strip()
-        book.author = author.strip()
-        book.title  = title.strip()
-        book.year   = year.strip()
-        book.status = status
-        return book
-
-
-    def add_base(book):
-        if type(book) == Book: book = json.encoder(book)
-        elif type(book) == dict:
-            book["status"] = "FREE"
-            if not list(book.keys()) == ["genre", "author", "title", "year", "status"]: raise ValueError()
-        else: raise TypeError()
-
-        if not path.exists("book_database.json"):
-            database = {1: book}
+class book:
+    def create(genre=str, author=str, title=str, year=int, *status):
+        if any([type(k) != str for k in [genre, author, title]]) or type(year)!=int: raise TypeError()
+        
+        if not len(status) in [0, 1]: raise ValueError()
         else:
-            with open("book_database.json", "r") as file:
-                database = load(file)
-                database[int(max(database.keys()))+1] = book
+            if len(status) == 1 and list(status)[0].lower() in ["free", "taken"]: raise ValueError()
+
+        return {"genre": genre, 
+                "author": author, 
+                "title": title, "year": year, 
+                "status": "FREE" if len(status) == 0 else list(status)[0].upper()
+                    }
+    
+class user:
+    def create(first_name='', last_name='', contacts={}, login='', password=''):
+        if any([type(k)!=str for k in [first_name, last_name, login, password]]) or type(contacts)!=dict: raise TypeError
+        if all([k not in ["email", "phone"] for k in contacts.keys()]): raise ValueError()
+
+        return {"first_name": first_name, 
+                "last_name":  last_name, 
+                "email": contacts["email"] if "email" in contacts.keys() else None,
+                "phone": contacts["phone"] if "phone" in contacts.keys() else None,
+                "login": login,
+                "password": hash(password, "encrypt"),
+                "books": []
+                    }
+    
+
+    def add_book(user_id, book_id):
+        if any([type(k)!=int] for k in [user_id, book_id]): raise TypeError()
+        elif user_id<1 or book_id<1: raise ValueError()
+        books = load(open("user_database.json", "r"))[user_id]["books"]
+        database.edit("user", user_id, books=books+[book_id])
+        database.edit("book", book_id, status="TAKEN")
+    
+
+    def ban(id):
+        user = load(open("user_database.json", "r"))[str(id)]
+        database.delete("user", id)
+        database.add(user, "banned")
+
+class database:
+    def add(object={}, category=''):
+        if list(map(type, [object, category])) != [dict, str]: raise TypeError()
+        elif any([len(k)==0 for k in [object, category]]): raise ValueError()
+
+        if not path.exists(f"{category}_database.json"):
+            database = {"1": object}
+        else:
+            database = load(open(f"{category}_database.json", "r"))
+            database[int(max(database.keys()))+1] = object
         
-        with open("book_database.json", "w") as file:
-            dump(database, file, indent=4, ensure_ascii=False)
+        with open(f"{category}_database.json", "w") as file: dump(database, file, indent=4, ensure_ascii=False)
 
 
-    def edit_book(id, **kwargs):
+    def edit(category='', id=0, **changes):
+        if list(map(type, [category, id, changes])) != [str, int, dict]: raise TypeError()
+        elif category == "" or not id > 0 or len(changes) == 0: raise ValueError()
         id = str(id)
-        if any([k not in ["genre", "author", "title", "year"] for k in kwargs.keys()]): raise ValueError()
+
+        if not path.exists(pth:=f"{category.lower()}_database.json"): raise ValueError()
+        database = load(open(pth, "r"))
+                
+        changes = {k.lower(): changes[k] for k in changes.keys() if k in database[id].keys()}
+        if len(changes) == 0: raise ValueError()
+        for attribute in changes.keys():
+            database[id][attribute] = changes[attribute]
         
-        with open("book_database.json", "r") as file:
-            book = (database:=load(file))[id]
-
-        book["genre"]  = kwargs["genre"] if "genre" in kwargs.keys() else book["genre"]
-        book["author"] = kwargs["author"] if "author" in kwargs.keys() else book["author"]
-        book["title"]  = kwargs["title"] if "title" in kwargs.keys() else book["title"]
-        book["year"]   = kwargs["year"] if "year" in kwargs.keys() else book["year"]
-        
-        database[id] = book
-
-        with open("book_database.json", "w") as file:
-            dump(database, file, indent=4, ensure_ascii=False)
+        with open(pth, "w") as file: dump(database, file, indent=4, ensure_ascii=False)
 
 
-    def st_change(id, **kwargs):
-        if not id > 0: raise ValueError()
+    def delete(category='', id=0):
         id = str(id)
-        if not list(kwargs.keys()) == ["status"]: raise ValueError()
-        elif not type(kwargs["status"]) == str: raise TypeError()
-        elif not kwargs["status"].lower() in ["free", "taken"]: raise ValueError()
+        if not path.exists(pth:=f"{category}_database.json"): raise ValueError()
 
-        with open("book_database.json", "r") as file:
-            book = (database:=load(file))[id]
-        
-        book["status"] = kwargs["status"].upper()
-        database[id] = book
-
-        with open("book_database.json", "w") as file:
-            dump(database, file, indent=4, ensure_ascii=False)
-
-
-    def del_base(id):
-        if not id > 0: raise ValueError()
-        id = str(id)
-        if not path.exists("book_database.json"): raise ValueError()
-        with open("book_database.json", "r") as file:
-            database = load(file)
+        database = load(open(pth, "r"))
         if not id in database.keys(): raise ValueError()
-        del database[id]
-        with open("book_database.json", "w") as file:
-            dump(database, file, indent=4, ensure_ascii=False)
+        database.remove(id)
+        
+        with open(pth, "w") as file: dump(database, file, indent=4, ensure_ascii=False)
